0d5cf9d5d6113b0a58d54899d0b42402889dd9fc
Jun Kawahara <snade.jun.kawahara@gmail.com>
2020-12-11 00:32:15 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -18,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "メールを送信しました。メールを確認して、有効化して下さい。"
+      redirect_to root_url
     else
       render 'new'
     end
@@ -51,15 +52,6 @@ class UsersController < ApplicationController
   end
   
   # beforeアクション
-
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
   
   # 正しいユーザーかどうか確認
   def correct_user
